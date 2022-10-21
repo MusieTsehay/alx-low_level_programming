b66ceb9019c86d25be1cc474ce5bb7a8d23e4b73
musietsehay <musietsehay@gmail.com>
2022-10-20 20:36:54 -0700

    Exe-03

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point of the code
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char T[9] = "_putchar\n";
+int i = 0;
+
+for (i = 0; i < 9; i++)
+{
+_putchar(T[i]);
+}
+return (0);
+}
