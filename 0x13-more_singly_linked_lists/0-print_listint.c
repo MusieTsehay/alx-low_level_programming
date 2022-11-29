3c5b961f6b627ad741e2924dfdd209de299d5010
musietsehay <musietsehay@gmail.com>
2022-11-29 03:48:32 -0800

    q1_3

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,17 +9,15 @@
  *      */
 size_t print_listint(const listint_t *h)
 {
-		size_t n_nodes = 0;
-
-			if (!h)
-						return (0);
-
-				while (h)
-						{
-									printf("%d\n", h->n);
-											h = h->next;
-													n_nodes++;
-														}
-					return (n_nodes);
+ size_t n_nodes = 0;
+ if (!h)
+	return (0);
+ while (h)
+{
+	printf("%d\n", h->n);
+	h = h->next;
+	n_nodes++;
+}
+ return (n_nodes);
 }
 
