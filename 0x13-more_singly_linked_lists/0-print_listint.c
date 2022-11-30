3b41f688c0e559a5f5ed698f53d426e806ad8316
musietsehay <musietsehay@gmail.com>
2022-11-29 22:56:16 -0800

    create single list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,7 +6,8 @@
  *   * @h: pointer to the head of a list.
  *    *
  *     * Return: Number of elements in a list.
- *      */
+ *     
+ */
 size_t print_listint(const listint_t *h)
 {
  size_t n_nodes = 0;
@@ -14,7 +15,7 @@ size_t print_listint(const listint_t *h)
 	return (0);
  while (h)
 {
-	printf("%d\n", h->n);
+	/*printf("%d\n", h->n);*/
 	h = h->next;
 	n_nodes++;
 }
